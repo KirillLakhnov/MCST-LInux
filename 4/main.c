2f4714b80fbdc887e7857e20562e9b7b012ea6d0
KirillLakhnov <112010038+KirillLakhnov@users.noreply.github.com>
2024-05-19 02:47:47 +0300

    Correction of minor errors

--- 4/main.c
+++ 4/main.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
     if(argc <= 1)
     {
         printf("\e[31mНе переданы аргументы командной строки\e[0m");
+        return 1;
     }
 
     parser(argc, argv);
--- 6/main.c
+++ 6/main.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
     if(argc <= 1)
     {
         printf("\e[31mНе передано имя файла, информацию о котором надо получить\e[0m");
+        return 1;
     }
 
     get_background_info_file(argv[1]);
